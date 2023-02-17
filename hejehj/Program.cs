0d8ab6c8a749c8b0fadb736fdfd6dfc1fda7d011
japo04001 <Admin@TEB12705>
2023-02-17 10:08:16 +0100

    Add project files.

---
+++ hejehj/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Metod1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv två heltal för att få fram arean på en rektangel");
+            Console.Write("Tal 1: ");
+            int tal1 = int.Parse(Console.ReadLine());
+            Console.Write("Tal 2: ");
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Arean för rektangel med sidorna {tal1} och {tal2} blir {Area(tal1, tal2)}");
+        }
+
+        static double Area(int tal1, int tal2)
+        {
+            return tal1 * tal2;
+        }
+
+    }
+}
